33ca0dde50cdf17e70e78b8635a85bd489e4c93b
blacksonic <soos.gabor86@gmail.com>
2016-03-10 11:43:29 +0100

    feat(error): add constructor data attribute to property instead of extending itself

--- requestError.js
+++ requestError.js
@@ -1,17 +1,11 @@
 'use strict';
 
-var _ = require('lodash');
-
 var SuiteRequestError = function(message, code, data) {
   this.message = message;
   this.name = 'SuiteRequestError';
   this.code = code;
-  this.data = {
-    replyText: message
-  };
+  this.data = data;
   this.stack = new Error().stack;
-
-  _.extend(this, data);
 };
 
 SuiteRequestError.prototype = Object.create(Error.prototype);
---
+++ requestError.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var SuiteRequestError = require('./requestError');
+
+describe('SuiteRequestError', function() {
+  it('should extend base Error class', function() {
+    var error = new SuiteRequestError();
+
+    expect(error).to.be.an.instanceOf(Error);
+  });
+
+  it('should store constructor parameters', function() {
+    var error = new SuiteRequestError('Invalid request', 400, { detailedMessage: 'Line too long' });
+
+    expect(error.message).to.eql('Invalid request');
+    expect(error.code).to.eql(400);
+    expect(error.data).to.eql({ detailedMessage: 'Line too long' });
+  });
+});

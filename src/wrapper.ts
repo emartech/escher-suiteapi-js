dd4bf82c35df287b1e582c2d6d16cc39d505bd06
Boris Tomic <16724532+boristomic@users.noreply.github.com>
2023-12-11 15:52:08 +0100

    Revert "chore: ensure headers are case insensitive"
    This reverts commit b7685540631d2eda72648a6bbae15a2e3bf5585e.

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -155,16 +155,8 @@ export class RequestWrapper {
   }
 
   private isJsonResponse<T extends TransformedResponse | AxiosResponse>(response: T) {
-    let headers: RawAxiosResponseHeaders | AxiosResponseHeaders = {};
-    Object.assign(headers, response.headers);
-    headers = Object.entries(response.headers)
-      .reduce((acc: RawAxiosResponseHeaders | AxiosResponseHeaders, [key, val]) => {
-        acc[key.toLowerCase()] = val.toLowerCase();
-        return acc;
-      }, ({}));
-
-    return headers['content-type'] &&
-      headers['content-type'].indexOf('application/json') !== -1;
+    return response.headers['content-type'] &&
+      response.headers['content-type'].indexOf('application/json') !== -1;
   }
 
   private getLogParameters(extraParametersToLog = {}) {
